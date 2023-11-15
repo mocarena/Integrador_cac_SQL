36cf9b0ae01e18f4d64a53f91f98c98a5d46348a
mocarena <147240281+mocarena@users.noreply.github.com>
2023-11-15 07:24:43 -0300

    Add files via upload

--- Integrador_CAC_Moni_Carena/integrador_cac_routines.sql
+++ Integrador_CAC_Moni_Carena/integrador_cac_routines.sql
@@ -16,6 +16,14 @@ USE `integrador_cac`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping events for database 'integrador_cac'
+--
+
+--
+-- Dumping routines for database 'integrador_cac'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +34,4 @@ USE `integrador_cac`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-15  6:11:22
+-- Dump completed on 2023-11-15  7:22:48
